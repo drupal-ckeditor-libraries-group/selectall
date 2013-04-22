671331ad788631cf90f18775b1261ad1f677cb89
Wiktor Walc <w.walc@cksource.com>
2013-04-22 16:29:28 +0200

    Updated language files, added Albanian translation.

--- lang/sq.js
+++ lang/sq.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'selectall', 'fr-ca', {
-	toolbar: 'Sélectionner tout'
+CKEDITOR.plugins.setLang( 'selectall', 'sq', {
+	toolbar: 'Përzgjidh të Gjitha'
 });
